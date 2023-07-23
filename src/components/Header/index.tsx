cae14eebdc47462de19f8d402295c77ce35f02e1
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-07-23 17:13:03 -0300

    feat: add summary cards style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <NewTransactionButton>New transaction</NewTransactionButton>
       </HeaderContent>
     </HeaderContainer>
   );
