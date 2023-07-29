9556938b72f3eb42203769a67f5237228a5aaad3
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-07-29 19:22:19 -0300

    feat: add utils formatter function to date and price

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionsTable,
 } from "./styles";
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
 
 export function Transactions() {
   const { transactions} = useContext(TransactionsContext);
@@ -26,10 +27,13 @@ export function Transactions() {
                   <tr key={transaction.id}>
                   <td width={"50%"}>{transaction.description}</td>
                   <td>
-                    <PriceHighlight variant="income">{transaction.price}</PriceHighlight>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                      </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
                 )
               }
